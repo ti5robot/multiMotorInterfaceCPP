00feed602d90d63b30bfbea09d90654584fbbc45
Mrhouse <Mrhouse.ma@outlook.com>
2024-09-02 16:11:56 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,38 @@
 /*多电机，位置接口，状态接口，暂停接口，恢复接口 C++版*/
+#include <stdio.h>
+#include <cstring>
+#include <stdlib.h>
+#include<sstream>
+#include<fstream>
+#include<cmath>
+#include<iostream>
+#include <unistd.h>
+#include <cstdlib>
+#include <signal.h>
 #include "Ti5BASIC.h"
 using namespace std;
 int main()
 {
-    allocate_variable(6);
+    setIDNUM(2);
+    cout<<"IDNUM="<<IDNUM<<endl;
+    allocate_variable(IDNUM);
 
-    login();
-    cout << "登录成功" << endl;
-    get_elc_info(6);
-    int q = get_elektrische_Maschinen_status(6);
-    clear_elc_error(6);
-    set_elc_info(ampere, 0, MotorTypeHelper::REG_TARGET_CURRENT, 0);
-    TH.j[0]=1.2;
-    move_to_joint();
+    if(login()){
+        cout<<"login success"<<endl;
+    }
+    
+    //get_elc_info(IDNUM);
+    //int q = get_elektrische_Maschinen_status(IDNUM);
+    //clear_elc_error(IDNUM);
+    //set_elc_info(ampere, 0, MotorTypeHelper::REG_TARGET_CURRENT, 0);
+    //TH.j[0]=1.2;
+    //move_to_joint();
+    float arr2[2]={2.2,2.2};
+    joint_movement(arr2);
     deallocate_variable();
-    logout();
+    if(logout()){
+        cout<<"logout success"<<endl;
+    }
 
     return 0;
 }
